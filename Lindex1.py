caad3308e03b7b676b4d48235a4e202c38fa9769
Marcelo Caparotta <49997117+doesm@users.noreply.github.com>
2022-09-23 19:35:25 -0400

    Add files via upload

---
+++ Lindex1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 19:12:56 2019
+
+@author: marcelo
+"""
+import argparse
+import subprocess
+import pandas as pd
+import math
+
+parser = argparse.ArgumentParser()
+parser.add_argument('trajectory', help='XTC Trajectory')
+parser.add_argument('start', type=int, help='First atom')
+parser.add_argument('end', type=int, help='Last atom')
+args = parser.parse_args()
+
+dv=""
+dp=""
+with open('lindex.dat', 'w') as f:
+    for i in range(args.start,args.end+1):
+        for j in range(args.start,args.end+1):
+            if j != i:
+                f.write('d'+str(i)+'-'+str(j)+': DISTANCE ATOMS=' + str(i) + ',' + str(j) + ' NOPBC\n')
+                f.write('p'+str(i)+'-'+str(j)+': CUSTOM ARG=d'+str(i)+'-'+str(j)+' FUNC=x*x PERIODIC=NO\n')
+                if j == args.start:
+                    dv='d'+str(i)+'-'+str(j)
+                    dp='p'+str(i)+'-'+str(j)
+                else: 
+                    dv=dv+',d'+str(i)+'-'+str(j)
+                    dp=dp+',p'+str(i)+'-'+str(j)
+        f.write('PRINT ARG='+dv+' FILE=dv'+str(i)+'\n')
+        f.write('PRINT ARG='+dp+' FILE=dp'+str(i)+'\n')
+f.closed
+
+subprocess.run(["plumed", "driver", "--plumed", "lindex.dat", "--mf_xtc", args.trajectory])
+
+sumj=0
+sumqi=0
+for i in range(args.start,args.end+1):
+    #print(i)
+    df=pd.read_csv('dv'+str(i)+'', header=None, skiprows=1, skipinitialspace=True, index_col=0, sep='\s+', dtype=float)
+    pf=pd.read_csv('dp'+str(i)+'', header=None, skiprows=1, skipinitialspace=True, index_col=0, sep='\s+', dtype=float)            
+    for j in range(args.start,args.end):
+        #print(j)
+        sumj=sumj+math.sqrt(pf.mean(axis=0)[j]-df.mean(axis=0)[j]**2)/df.mean(axis=0)[j]
+    qi=1/((args.end+1-args.start)-1)*sumj
+    print("Atom ", i," = ",qi)
+    sumj=0
+    sumqi=sumqi+qi
+
+q=1/(args.end+1-args.start)*sumqi
+print("System-averaged Lindemann index: ", q)
